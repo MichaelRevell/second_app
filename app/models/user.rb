d048ce2851996de4b6bfd0297f4327c2b69f05e0
Michael <mars8082686@gmail.com>
2011-11-14 16:31:09 +0900

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,28 +27,29 @@ class User < ActiveRecord::Base
                       :confirmation => true,
                       :length       => { :within => 6..40 }
                       
-                      before_save :encrypt_password
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
 
-                        def has_password?(submitted_password)
-                          encrypted_password == encrypt(submitted_password)
-                        end
+   private
 
-                        private
+     def encrypt_password
+       self.salt = make_salt unless has_password?(password)
+       self.encrypted_password = encrypt(password)
+     end
 
-                          def encrypt_password
-                            self.salt = make_salt unless has_password?(password)
-                            self.encrypted_password = encrypt(password)
-                          end
-
-                          def encrypt(string)
-                            secure_hash("#{salt}--#{string}")
-                          end
-
-                          def make_salt
-                            secure_hash("#{Time.now.utc}--#{password}")
-                          end
-
-                          def secure_hash(string)
-                            Digest::SHA2.hexdigest(string)
-                          end
+     def encrypt(string)
+       secure_hash("#{salt}--#{string}")
+     end
+     
+     def make_salt
+       secure_hash("#{Time.now.utc}--#{password}")
+     end
+     
+     def secure_hash(string)
+       Digest::SHA2.hexdigest(string)
+     end
 end
