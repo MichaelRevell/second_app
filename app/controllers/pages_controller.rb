ac1dfe3f7878227b0688b1d841f7dbcbb2211e84
Michael <mars8082686@gmail.com>
2011-11-29 00:56:33 +0900

    added destroy micropost

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   def home
     @title = "Home"
+    @micropost = Micropost.new if signed_in?
+    @user = current_user if signed_in?
+    @microposts = @user.microposts.paginate(:page => params[:page])
   end
 
   def contact
